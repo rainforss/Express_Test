6fe1b6dda787fd71f4b71e400505938ba09ffbf1
rainforss <shengyan@ualberta.ca>
2020-08-31 21:17:10 -0600

    test 1

---
+++ Members.js
@@ -0,0 +1,7 @@
+const members = [
+  { id: 1, name: "Billy Hope", email: "billy@gmail.com", status: "deceased" },
+  { id: 2, name: "Miles Upshur", email: "miles@gmail.com", status: "active" },
+  { id: 3, name: "Allan Tores", email: "allan@gmail.com", status: "active" },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require("express");
-
+const path = require("path");
+const exphbs = require("express-handlebars");
 const app = express();
+const moment = require("moment");
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUrl
+    }: ${moment().format()}`
+  );
+  next();
+};
+
+//bodyParser
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+//handlebars
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// app.use(logger);
+
+app.use("/api/members", require("./routes/api/members"));
+
+//Static folder
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
